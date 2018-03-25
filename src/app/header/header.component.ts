4566ea77fa43485c69ab065e6dc516a1d9706de1
lalit527 <lalit.slg007@gmail.com>
2018-03-25 08:56:23 +0530

    Added bootstrap changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
   data:string = "Two Way";
