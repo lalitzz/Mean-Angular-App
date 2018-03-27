607538fae220cfb90f6bde7a4b1f6631fb44e9bc
lalit527 <lalit.slg007@gmail.com>
2018-03-27 18:08:28 +0530

    Added component for showing tweets from service

---
+++ src/app/tweet/tweet.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { TweetService } from '../services/tweet.service';
+
+@Component({
+  selector: 'app-tweet',
+  templateUrl: './tweet.component.html',
+  styleUrls: ['./tweet.component.scss']
+})
+export class TweetComponent implements OnInit {
+  tweetData: Array<Object>; 
+  constructor(private tweets: TweetService) { }
+
+  ngOnInit() {
+    this.tweetData = this.tweets.getTweet();
+  }
+
+}
