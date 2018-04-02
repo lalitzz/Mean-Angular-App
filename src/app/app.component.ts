a426b7944cdc688b6e37135ce352377105fcd6e3
lalit527 <lalit.slg007@gmail.com>
2018-04-02 17:56:00 +0530

    Showing sidebar after login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component
         , OnInit
  } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,12 @@ import { Component
 export class AppComponent implements OnInit {
   title = 'app';
   hello = 'This should Work';
+  isLogged = false;
+  constructor(private logService: AuthService) {}
   ngOnInit(){
-
+    this.logService.getAuthStatus().subscribe(data => {
+      this.isLogged =data;
+    });
   }
   countChange(event) {
     //console.log(event);
